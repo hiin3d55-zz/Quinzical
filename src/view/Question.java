458d1ea4d18a65eed61eb4c30bbc68c0791afd43
Dave Shin <jshi911@en416253.uoa.auckland.ac.nz>
2020-09-28 16:07:35 +1300

    Finished GamesModule.

--- src/view/Question.java
+++ src/view/Question.java
@@ -8,17 +8,25 @@ public class Question {
 
 	private int _amount;
 	private String _clue;
+	private String _category;
 	private Button _clueBtn;
+	private boolean _isMin;
 	
-	public Question(int amount, String clue) {
+	public Question(int amount, String clue, String category) {
 		_amount = amount;
 		_clue = clue;
-		_clueBtn = new Button("$" + String.valueOf(_amount));
+		_category = category;
+		_clueBtn = new Button("$" + String.valueOf(amount));
+		if (amount == 100) {
+			_isMin = true;
+		} else {
+			_isMin = false;
+		}
 		
 		_clueBtn.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
-				if (amount == 100) {
-				System.out.println("hello hi");
+				if (_isMin) {
+					System.out.println("hello hi");
 				}
 			}
 		});
@@ -30,5 +38,17 @@ public class Question {
 	
 	public Button getButton() {
 		return _clueBtn;
+	}
+	
+	public boolean checkIfItIsMin() {
+		return _isMin;
+	}
+	
+	public String getCategory() {
+		return _category;
+	}
+	
+	public void nowMin() {
+		_isMin = true;
 	}
 }
