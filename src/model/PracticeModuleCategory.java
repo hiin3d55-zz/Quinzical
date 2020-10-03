bef0463aad276902bee9bfd6af374ca64a3c668a
Sherman <shermanchin33@gmail.com>
2020-10-03 18:42:59 +1300

    Add comments

--- src/model/PracticeModuleCategory.java
+++ src/model/PracticeModuleCategory.java
@@ -1,7 +1,16 @@
 package model;
 import java.io.File;
 
+/**
+ * This class represents functionalities that retrieves categories for Practice Module.
+ * @author se2062020
+ *
+ */
 public class PracticeModuleCategory extends Category{
+	
+	/**
+	 * Gets all the categories available in the question bank.
+	 */
 	public String[] getCategories() {
 		File file = new File("questionBank");
 		return file.list();
--- src/model/PracticeModuleClue.java
+++ src/model/PracticeModuleClue.java
@@ -1,11 +1,23 @@
 package model;
 
+/**
+ * This class represents functionalities to retrieve clues for Practice Module.
+ * @author se2062020
+ *
+ */
 public class PracticeModuleClue extends Clue{
+	
+	/**
+	 * Get all the clues that are in a category from the question bank.
+	 */
 	public String[] getClues(String category) {
 		String[] clues = getAllClues(category);
 		return randomiseClues(clues, 1);
 	}
 
+	/**
+	 * This method does nothing as it is intended for GamesModuleClue
+	 */
 	public int[] getClueValues(String category) {
 		return null;
 	}
